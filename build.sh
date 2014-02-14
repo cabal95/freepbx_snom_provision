ce088f841c571ceedea5b89cfa759f4811107fc3
Daniel Hazelbaker <daniel@blueboxmoon.com>
2014-02-13 22:16:29 -0800

    Fix incorrect md5 calculation.

--- build.sh
+++ build.sh
@@ -12,10 +12,11 @@ for d in `find . -depth 1 -type d | cut -c3-`; do
   if [ $? -eq 0 ]; then
     old_md5=`tar -tzvf "$d".tgz 2>/dev/null | md5 -r | cut -f1 -d" " 2>/dev/null`
     new_md5=`tar -tzvf temporary.tgz | md5 -r | cut -f1 -d" "`
+    real_md5=`md5 -r temporary.tgz | cut -f1 -d" "`
 
     if [ "$old_md5" != "$new_md5" ]; then
       mv temporary.tgz "$d".tgz
-      cat template_data.json | sed "s/__MD5SUM__/$new_md5/g" | sed "s/__LASTMOD__/$last_mod/g" >"$d".json
+      cat template_data.json | sed "s/__MD5SUM__/$real_md5/g" | sed "s/__LASTMOD__/$last_mod/g" >"$d".json
       git add "$d".tgz "$d".json
       updated=1
     fi
